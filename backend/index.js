cc672ff3614a8bf06fa594053c7553bf838c8a49
Joyin2 <joyinlaskar2000@gmail.com>
2022-11-11 11:10:00 +0530

    index

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,10 @@ const app = express();
 
 app.use(express.json())
 
-app.post("/register", (req, res) => {
-  res.send(req.body);
+app.post("/register", async(req, res) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  res.send(result);
 });
 
 app.listen(4200);
