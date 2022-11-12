b34a1445610462e1c58e9f03144b8a4621fb5d96
Joyin2 <joyinlaskar2000@gmail.com>
2022-11-12 10:56:44 +0530

    install cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/config");
 const User = require("./db/Users");
 const app = express();
 
 app.use(express.json())
+app.use(cors());
 
 app.post("/register", async(req, res) => {
   let user = new User(req.body);
